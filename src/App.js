ac5357cb3dcba11957c1c6b39f41f6dd11000de9
DavidLichenko <dlichenko1324@gmail.com>
2022-11-08 18:26:11 +0000

    testing#2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
 import { Effects, OrbitControls, OrthographicCamera, BakeShadows, Html } from '@react-three/drei'
 import { UnrealBloomPass } from 'three-stdlib'
 import './App.css'
-import B12 from './components/three/models/B122'
+import B12 from './components/three/models/B12'
 import Text from './components/textThree/Text'
 
 extend({ UnrealBloomPass })
@@ -15,14 +15,15 @@ const CameraController = () => {
     const controls = useRef();
     const { camera, scene } = useThree();
     const { children } = scene
-    const object = children[0]
+    const object = children[1]
+    console.log(object)
     useFrame(state => {
         // let changeX = state.mouse.x - oldX;
         // let changeY = state.mouse.y - oldY;
         // camera.position.x = changeX
         // camera.position.y = changeY
-        object.rotation.x = -state.mouse.y/1.2
-        object.rotation.y = -state.mouse.x/1.2
+        object.rotation.x = -state.mouse.y/2
+        object.rotation.y = -state.mouse.x/2
 
     })
 }
@@ -41,23 +42,23 @@ export default function App() {
     return (
         <>
 
-                <Canvas shadowMap shadows gl={{ antialias: false }}>
-                    <CameraController/>
-                    <color attach="background" args={['#ffffff']} />
-                    {/*<fog attach="fog" args={['#000000', 0, 60]} />*/}
-                    {/*<hemisphereLight intensity={.9} color="#eaeaea" groundColor="black" />*/}
-                    <B12/>
-                    <ambientLight intensity={0.1} />
-                    <Box color="black" position={[-2, 2, 8]} />
-                    <Text/>
-                    <directionalLight intensity={9}  position={[10, 10, -10]} />
-                    {/*<Effects >*/}
-                    {/*    <unrealBloomPass threshold={1} strength={1.0} radius={0.5} />*/}
-                    {/*</Effects>*/}
-                    {/*<BakeShadows />*/}
-                    <OrthographicCamera makeDefault far={90} near={-90}  position={[0, 0, -8]} zoom={90} />
-                    <OrbitControls  enableZoom={true} />
-                </Canvas>
+            <Canvas  shadows gl={{ antialias: false }}>
+                <CameraController/>
+                <color attach="background" args={['#d08b8b']} />
+                {/*<fog attach="fog" args={['#000000', 0, 60]} />*/}
+                <hemisphereLight intensity={.2} color="#000000" groundColor="white" />
+                <B12/>
+                {/*<ambientLight intensity={0.1} />*/}
+                <Box color="gray" position={[-2, 2, 18]} />
+                <Text/>
+                <directionalLight intensity={1}  position={[10, 10, -10]} />
+                {/*<Effects >*/}
+                {/*    <unrealBloomPass threshold={1} strength={1.0} radius={0.5} />*/}
+                {/*</Effects>*/}
+                {/*<BakeShadows />*/}
+                <OrthographicCamera makeDefault far={90} near={-90}  position={[0, 0, -8]} zoom={90} />
+                <OrbitControls enablePan={true} enableRotate={true} enableZoom={true} />
+            </Canvas>
 
         </>
     );
