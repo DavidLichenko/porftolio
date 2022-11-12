9019a3e2a6a31af9b66be366da02d387d5ba3678
DavidLichenko <dlichenko1324@gmail.com>
2022-11-12 13:39:24 +0000

    testing#3

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import React, { useRef, useEffect } from "react";
+
+import React, { useRef,useState } from "react";
 import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
-import { Effects, OrbitControls, OrthographicCamera, BakeShadows, Html } from '@react-three/drei'
+import { Effects, OrbitControls, OrthographicCamera } from '@react-three/drei'
 import { UnrealBloomPass } from 'three-stdlib'
 import './App.css'
 import B12 from './components/three/models/B12'
+import Ruin from './components/three/models/RobotInRuinScene'
 import Text from './components/textThree/Text'
 
 extend({ UnrealBloomPass })
 
 
-let oldX = 0;
-let oldY = 0;
-const CameraController = () => {
+function CameraController()  {
     const controls = useRef();
+
     const { camera, scene } = useThree();
-    const { children } = scene
-    const object = children[1]
+    const object = scene.getObjectByName("B12")
     console.log(object)
     useFrame(state => {
-        // let changeX = state.mouse.x - oldX;
-        // let changeY = state.mouse.y - oldY;
-        // camera.position.x = changeX
-        // camera.position.y = changeY
-        object.rotation.x = -state.mouse.y/2
-        object.rotation.y = -state.mouse.x/2
+        let changeX = state.mouse.x - 18;
+        let changeY = state.mouse.y + 6;
+        camera.position.z = changeX/5
+        camera.position.y = changeY/5
+        object.rotation.x = -1.5707963267948966 + -state.mouse.y/4
+        object.rotation.y = -state.mouse.y/2
+        object.rotation.z = -3.141592653589793 + -state.mouse.x/2
 
     })
 }
+
+
+
 function Box({position, color}) {
     const ref = useRef()
     return (
@@ -39,25 +43,31 @@ function Box({position, color}) {
 
 export default function App() {
 
+
+    const [Light,setLight] = useState(0)
     return (
         <>
 
-                <Canvas  shadows gl={{ antialias: false }}>
+                <Canvas
+                    onClick={(e) => {
+                        setLight(Light ? 0 : 8)
+                    }}
+                    shadows dpr={[1,2]} gl={{ antialias: false }}>
                     <CameraController/>
-                    <color attach="background" args={['#d08b8b']} />
-                    {/*<fog attach="fog" args={['#000000', 0, 60]} />*/}
-                    <hemisphereLight intensity={.2} color="#000000" groundColor="white" />
-                    <B12/>
-                    {/*<ambientLight intensity={0.1} />*/}
-                    <Box color="gray" position={[-2, 2, 18]} />
+                    {/*<color attach="background" args={['#d08b8b']} />*/}
+                    <fog attach="fog" args={['#006477', 0, 30]} />
+                    {/*<hemisphereLight intensity={1} color="#000000" groundColor="black" />*/}
+                    <B12 Light={Light} setLight={setLight}/>
+                    <ambientLight intensity={0.05} />
+                    <Ruin/>
+                    {/*<Box color="black" position={[-2, 2, 18]} />*/}
                     <Text/>
-                    <directionalLight intensity={1}  position={[10, 10, -10]} />
+                    {/*<directionalLight intensity={1}  position={[10, 10, -10]} />*/}
                     {/*<Effects >*/}
-                    {/*    <unrealBloomPass threshold={1} strength={1.0} radius={0.5} />*/}
+                    {/*    <unrealBloomPass threshold={.8} strength={1} radius={.01} />*/}
                     {/*</Effects>*/}
-                    {/*<BakeShadows />*/}
-                    <OrthographicCamera makeDefault far={90} near={-90}  position={[0, 0, -8]} zoom={90} />
-                    <OrbitControls enablePan={true} enableRotate={true} enableZoom={true} />
+                    {/*<OrthographicCamera makeDefault far={90} near={-90}  position={[0, 0, -8]} zoom={90} />*/}
+                    {/*<OrbitControls enablePan={true} enableRotate={true} enableZoom={true} />*/}
                 </Canvas>
 
         </>
--- src/components/textThree/Text.js
+++ src/components/textThree/Text.js
@@ -8,9 +8,9 @@ extend({ TextGeometry })
 export default function Text() {
     const font = new FontLoader().parse(orbitron)
     return (
-        <mesh castShadow rotation={[-Math.PI / 30, 3, 0]} position={[2,0,10]}>
-            <textGeometry args={['Dovid {}O"|_|"O{} ' ,{font, size: 1, height: 0.1}]}/>
-            <meshPhysicalMaterial attach='material' metalness={2} roughness={.6} color={'dimgray'}/>
+        <mesh castShadow rotation={[0,-Math.PI/2, 0]} position={[8.35,4.56,-4.44]}>
+            <textGeometry args={['Dovid {}O"|_|"O{} ' ,{font, size: .3, height: 0.1}]}/>
+            <meshPhysicalMaterial attach='material' metalness={0} roughness={0} color={'black'}/>
         </mesh>
     )
 
